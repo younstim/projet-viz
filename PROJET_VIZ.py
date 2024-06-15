1560b28ff733ae34f30f64d07257162c4c94635b
younstim <lageekentreprise@gmail.com>
2024-06-15 19:11:03 +0200

    Update PROJET_VIZ.py

--- PROJET_VIZ.py
+++ PROJET_VIZ.py
@@ -229,7 +229,7 @@ plt.show()
 
 # Supprimer les informations après la date
 df['duedate'] = df['duedate'].str.extract(r'(\d{4}-\d{2}-\d{2})')[0]
-df['duedate'] = pd.to_datetime(df['duedate'], format=ISO8601)
+df['duedate'] = pd.to_datetime(df['duedate'], format='%Y-%m-%d')
 
 # Filtrer les stations qui ne sont pas en service
 df_not_installed = df[df['is_installed'] == "NON"].copy()
