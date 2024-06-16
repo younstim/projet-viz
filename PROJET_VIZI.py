9ef3159a6ab2f9ba876c1108ee399e21192dd256
younstim <lageekentreprise@gmail.com>
2024-06-16 17:33:11 +0200

    Update PROJET_VIZI.py

--- PROJET_VIZI.py
+++ PROJET_VIZI.py
@@ -296,13 +296,11 @@ plt.show()
 
 # Affichage du graphique
 st.pyplot(fig6)
-
 # Initialiser session_state pour les villes et types de vélos si elles n'existent pas
 if 'villes' not in st.session_state:
     st.session_state.villes = list(df['nom_arrondissement_communes'].unique())
 if 'types_velos' not in st.session_state:
     st.session_state.types_velos = ['mechanical', 'ebike']
-
 
 # Ajouter un titre au-dessus du formulaire
 st.markdown('<div class="stTitle">Répartition des vélos mécaniques et électriques par ville</div>', unsafe_allow_html=True)
@@ -313,7 +311,7 @@ with st.form(key='form1'):
     villes = st.multiselect(
         "Sélectionnez les villes",
         options=list(df['nom_arrondissement_communes'].unique()),
-        default=st.session_state.villes if st.session_state.villes else list(df['nom_arrondissement_communes'].unique())
+        default=st.session_state.villes
     )
 
     types_velos = st.multiselect(
@@ -328,7 +326,6 @@ if submit_button:
     # Mettre à jour les sélections dans session_state
     st.session_state.villes = villes
     st.session_state.types_velos = types_velos
-
 
 # Créer le graphique pour les vélos disponibles par commune
 if st.session_state.villes and st.session_state.types_velos:
